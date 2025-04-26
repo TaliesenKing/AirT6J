d8bfd9f7fb51bd251d956dcb40e26f00b255a883
TaliesenKing <taliesenking@yahoo.com>
2025-04-25 17:55:28 -0700

    troubleshooting

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,4 +1,6 @@
 const { sequelize } = require('./db/models');
+console.log(process.env.NODE_ENV)
+
 
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
   if (!data.includes(process.env.SCHEMA)) {

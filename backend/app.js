415a0259295026668733da8335b2b19b3845fe29
TaliesenKing <taliesenking@yahoo.com>
2025-04-25 18:03:11 -0700

    Yaseen to the rescuept3

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { ValidationError } = require('sequelize');
 
-const { environment } = require('./db/config/index');
+const { environment } = require('./config/index');
 const isProduction = environment === 'production';
 
 const routes = require('./routes');

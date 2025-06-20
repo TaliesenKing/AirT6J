b72614c3832aa5268911f97ce85103d50dc04de8
TaliesenKing <taliesenking@yahoo.com>
2025-06-19 18:17:06 -0700

    still working on lat and lng pt 3

--- backend/db/migrations/20250620010111-make-lat-lng-nullable.js
+++ backend/db/migrations/20250620010111-make-lat-lng-nullable.js
@@ -1,24 +1,46 @@
 'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.changeColumn('Spots', 'lat', {
-      type: Sequelize.FLOAT,
-      allowNull: true
-    });
-    await queryInterface.changeColumn('Spots', 'lng', {
-      type: Sequelize.FLOAT,
-      allowNull: true
-    });
+    await queryInterface.changeColumn(
+      { tableName: 'Spots', ...options },
+      'lat',
+      {
+        type: Sequelize.DECIMAL(10, 7),
+        allowNull: true,
+      }
+    );
+    await queryInterface.changeColumn(
+      { tableName: 'Spots', ...options },
+      'lng',
+      {
+        type: Sequelize.DECIMAL(10, 7),
+        allowNull: true,
+      }
+    );
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.changeColumn('Spots', 'lat', {
-      type: Sequelize.FLOAT,
-      allowNull: false
-    });
-    await queryInterface.changeColumn('Spots', 'lng', {
-      type: Sequelize.FLOAT,
-      allowNull: false
-    });
+    await queryInterface.changeColumn(
+      { tableName: 'Spots', ...options },
+      'lat',
+      {
+        type: Sequelize.DECIMAL(10, 7),
+        allowNull: false,
+      }
+    );
+    await queryInterface.changeColumn(
+      { tableName: 'Spots', ...options },
+      'lng',
+      {
+        type: Sequelize.DECIMAL(10, 7),
+        allowNull: false,
+      }
+    );
   }
 };
